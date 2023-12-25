0eb54e4f1ba3ec51d14769e6834f37f6bc224639
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-12-26 01:09:27 +0530

    chore: added hello service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get,App, HttpServer, Responder, HttpResponse};
+
+#[get("/")]
+async fn hello() -> impl Responder{
+    HttpResponse::Ok().body("Hello World!")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    match HttpServer::new(||App::new().service(hello)).bind(("127.0.0.1",8080)){Ok(server) =>{
+        println!("Server running on port 8080");
+        server.run().await
+    }Err(err) => {
+        println!("Error: {}", err);
+        Err(err)
+    }}
 }
